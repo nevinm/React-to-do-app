fcc416f15364f47e6967e76c3bbb3cf2240e4153
nevin <nevinm@qburst.com>
2016-04-15 15:35:32 +0530

    Added thunk/middleware to the app.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
-import { createStore }  from 'redux';
+import { applyMiddleware, compose, createStore }  from 'redux';
 import rootReducer  from './reducers';
+import thunk  from 'redux-thunk';
+
+let finalStore = compose(
+  applyMiddleware(thunk)
+)(createStore);
 
 export default function configureStore(InitialState = { todos: [], user: {} }) {
-  return createStore(rootReducer, InitialState,
+  return finalStore(rootReducer, InitialState,
      window.devToolsExtension ? window.devToolsExtension() : undefined);
 }
